1e83cdbac5cba6262cd01095e22010da57d2f26d
Harsh Desai <58596501+harshdesai695@users.noreply.github.com>
2025-01-11 11:03:40 +0530

    Discovery Service

---
+++ com.myKart.eureka/src/main/java/com/myKart/servicceDiscovery/EurekaApplication.java
@@ -0,0 +1,16 @@
+package com.myKart.servicceDiscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication extends SpringBootServletInitializer  {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
+
+}
